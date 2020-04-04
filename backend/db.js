bb211d101766e44293b6b09c516be5e528ea9c54
Doina Mihailov <doinamihailov@walhalla.local>
2020-04-04 13:40:09 +0300

    second commit

--- backend/db.js
+++ backend/db.js
@@ -10,3 +10,21 @@ const seq = new Sequelize(
     }
 )
 module.exports = seq;
+/*
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize(
+    'vat_0_1_dev',
+    'amber',
+    'XDR%6tfc', {
+        host: '109.100.42.38',
+        dialect: 'mysql',
+        logging: false,
+        pool: {
+            max: 10,
+            min: 0,
+            idle: 20000,
+            acquire: 20000
+        }
+    }
+);*/
